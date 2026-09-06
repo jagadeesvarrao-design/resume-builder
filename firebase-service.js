a460afdc4ab79eff9b41fe59ed963daa0e1aea12
jagadeesvarrao-design <jagadeesvarrao@gmail.com>
2026-09-06 18:26:24 +0530

    Use clean standard GoogleAuthProvider with select_account parameter

--- firebase-service.js
+++ firebase-service.js
@@ -356,9 +356,6 @@ window.triggerGoogleLogin = async function() {
   }
 
   const provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/userinfo.email');
-  provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
-  provider.addScope('openid');
   provider.setCustomParameters({ prompt: 'select_account' });
 
   try {
